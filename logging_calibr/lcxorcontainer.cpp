aaec0cf34fc9dbe3b0aba5aa077ebe603a5f2ce6
zxd <zxd>
2017-07-06 23:58:47 +0800

    add xcor function

--- logging_calibr/lcxorcontainer.cpp
+++ logging_calibr/lcxorcontainer.cpp
@@ -4,14 +4,18 @@
 #include "lcdefine.h"
 #include "lcmainwindow.h"
 #include "lcdata.h"
+#include "lcalgorithm.h"
+#include "ai_data_include.h"
+extern "C" void convolve_cwp(int lx, int ifx, float *x, int ly, int ify, float *y, int lz, int ifz, float *z);
+extern "C" void xcor(int lx, int ifx, float *x, int ly, int ify, float *y,	int lz, int ifz, float *z);
 LCXorContainer::LCXorContainer( QWidget *parent, Qt::WindowFlags f ) : QWidget( parent, f )
 {
 	ui.setupUi(this);
+	ui._lag_xor_chartview->setChart(new QChart());
 }
 
 LCXorContainer::~LCXorContainer()
 {}
-
 
 void LCXorContainer::onUpdate(const LCUpdateNotifier &update_notifier)
 {
@@ -31,15 +35,48 @@ void LCXorContainer::lagApplaySlot()
 
 void LCXorContainer::setLagXorChart()
 {
-	QChart *chart = ui._lag_xor_chartview->chart();
-	if (chart != nullptr) {
-		delete chart;
-	}
-	ui._lag_xor_chartview->setChart(new QChart());
 	QSettings &options = LCENV::MW->lcOptions();
 
 	int il_num = options.value("WellSeismic/ILTraceNum").toInt();
 	int xl_num = options.value("WellSeismic/XLTraceNum").toInt();
-	QVector<QPair<QVector<float>, QVector<float>>> all_trace_data = LCENV::MW->lcData()->wellSeismic(il_num, xl_num);
+	QVector<LCTrace> all_trace_data = LCENV::MW->lcData()->wellSeismic(il_num, xl_num);
+
+	aiDataWell *well_data = LCENV::MW->lcData()->wellData();
+
+	QVector<float> imped = fillInvalid(well_data->GetImpedance());
+	if (imped.size() == 0) {
+		QVector<float> sonic = fillInvalid(well_data->GetSonic());
+		QVector<float> rhob = well_data->GetDensity(); /* kg/m3 */
+		for (auto &item : rhob) {
+			if (isnan(item) == false) {
+				item /= 1000.f;
+			}
+		}
+		imped = impedance(sonic, rhob);
+	}
+	/* get wavelet */
+	LCWavelet wavelet = LCENV::MW->lcData()->currentWavelet();
+	aiDataSeismic *seis_data = LCENV::MW->lcData()->seismicData();
+	aiSampleRange sample_range = seis_data->GetSampleRange();
+	float sample_interval = sample_range.GetStep() / 1000.f; /* ms->s */
+	LCTimeDepthCurve time_depth_curve = LCENV::MW->lcData()->currentTimeDepthCurve();
+
+	LCTrace resample_imped = uniformResampleTimes(time_depth_curve.first, imped, sample_interval);
+	LCTrace resample_wavelet = uniformResampleTimes(wavelet.first, wavelet.second, sample_interval);
+
+	QVector<float> resample_refl = reflectivities(resample_imped.second);
+
+	int start_index = ui._lag_start_time_spinbox->value() / sample_range.GetStep();
+	int end_index = ui._lag_end_time_spinbox->value() / sample_range.GetStep();
+	int half_lag_num = ui._lag_range_spinbox->value() / sample_range.GetStep();
+	QVector<float> ext_resample_refl(resample_refl.size() + half_lag_num * 2);
+	ext_resample_refl.fill(0.f);
+
+	memcpy(&ext_resample_refl[half_lag_num], &resample_refl[0], resample_refl.size() * sizeof(float));
+	QVector<float> trace_out(resample_refl.size());
+	for (int index = 0; index <= half_lag_num * 2 + 1; index++) {
+		convolve_cwp(resample_refl.size(), index, ext_resample_refl.data(), resample_wavelet.second.size(), 0, resample_wavelet.second.data(), trace_out.size(), 0, trace_out.data());
+
+	}
 }
 
